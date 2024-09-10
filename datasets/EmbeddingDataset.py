3a7456eb5b1e1f4eec302a6f1b9dbdf7f0d38d3c
WeiYang <yang0sun@gmail.com>
2024-09-10 22:33:53 +0800

    update

--- datasets/EmbeddingDataset.py
+++ datasets/EmbeddingDataset.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Mar 20 22:08:03 2023
-
 @author: weiyang
 """
 
--- datasets/dataset.py
+++ datasets/dataset.py
@@ -1,17 +1,16 @@
 import torch
 
 class ProteinNode:
-    def __init__(self,ProteinID,ProteinLen,PrimarySeq,SecondarySeq,PHI,PSI,ACC):
+    def __init__(self,ProteinID,ProteinLen,PrimarySeq,SecondarySeq,PHI,PSI):
         self.PDB_ID=ProteinID
         self.ProteinLen=ProteinLen
         self.PrimarySeq=PrimarySeq
         self.SecondarySeq=SecondarySeq       
         self.PHI=PHI
-        self.PSI=PSI
-        self.ACC=ACC        
+        self.PSI=PSI               
         self.feats=None
 
-def load_FastaFile(FastaFile,IsESM=True):
+def load_FastaFile(FastaFile,IsESM=True, With_SS=True):
     f=open(FastaFile,'r')
     if IsESM:
         AA_Dict={ 'L':4, 'A':5, 'G':6, 'V':7, 'S':8, 'E':9, 'R':10, 'T':11, 'I':12, 'D':13, 'P':14, 'K':15, 'Q':16, 'N':17, 'F':18, 'Y':19, 'M':20, 'H':21, 'W':22,
@@ -33,77 +32,20 @@ def load_FastaFile(FastaFile,IsESM=True):
     while True:       
         PDBID=f.readline().strip()
         if len(PDBID)==0:
-            break
+            break        
         info=PDBID.split()
         ProteinID=info[0][1:]
-        PrimarySeq=f.readline().strip()
-        ProteinLen=len(PrimarySeq)
-        PrimarySeq=[AA_Dict[e] for e in PrimarySeq]
-        PrimarySeq=torch.tensor(PrimarySeq,dtype=torch.long)
-        SecondarySeq=f.readline().strip()
-        SecondarySeq=[SS9_Dict[e] for e in SecondarySeq]
-        SecondarySeq=torch.tensor(SecondarySeq,dtype=torch.long)        
-        PHI=f.readline().strip().split()
-        tmp_list=[]
-        for i in range(ProteinLen):
-            if PHI[i]!='X' and PHI[i][:3]!='360':
-                tmp_list.append(float(PHI[i]))
-            else:
-                tmp_list.append(float('nan'))
-        PHI=torch.tensor(tmp_list,dtype=torch.float)
-        PSI=f.readline().strip().split()
-        tmp_list=[]
-        for i in range(ProteinLen):
-            if PSI[i]!='X' and PSI[i][:3]!='360':
-                tmp_list.append(float(PSI[i]))
-            else:
-                tmp_list.append(float('nan'))
-        PSI=torch.tensor(tmp_list,dtype=torch.float)        
-        ACC=f.readline().strip().split()
-        tmp_list=[]
-        for i in range(ProteinLen):
-            if ACC[i]!='X':
-                tmp_list.append(float(ACC[i]))
-            else:
-                tmp_list.append(float('nan'))
-        ACC=torch.tensor(tmp_list,dtype=torch.float)                
-        Node=ProteinNode(ProteinID,ProteinLen,PrimarySeq,SecondarySeq,PHI/180.0,PSI/180.0,ACC)
-        Data.append(Node)
-    f.close()
-    return Data
-
-
-def load_FastaFile2(FastaFile,IsESM=True):
-    f=open(FastaFile,'r')
-    if IsESM:
-        AA_Dict={ 'L':4, 'A':5, 'G':6, 'V':7, 'S':8, 'E':9, 'R':10, 'T':11, 'I':12, 'D':13, 'P':14, 'K':15, 'Q':16, 'N':17, 'F':18, 'Y':19, 'M':20, 'H':21, 'W':22,
-                  'C':23, 'X':24, 'B':25, 'U':26, 'Z':27, 'O':28}
-    else:
-        #ProtTrans          
-        Standard_AAS=['A', 'L', 'G', 'V', 'S', 'R', 'E', 'D', 'T', 'I', 'P','K','F','Q', 'N', 'Y', 'M', 'H',  'W', 'C']
-        AA_Dict={}
-        Non_Standard_AAS=list('BJOUXZ')
-        for i,AA in enumerate(Standard_AAS):
-            AA_Dict.update({AA:i+3})
-        for i,AA in enumerate(Non_Standard_AAS):
-            AA_Dict.update({AA:23}) 
-    Data=[]
-    SS=['L','B','E','G','I','H','S','T','P','X']
-    SS9_Dict=dict(zip(SS,range(len(SS))))
-    SS9_Dict['X']=-100
-    SS9_Dict['C']=0
-    while True:       
-        PDBID=f.readline().strip()
-        if len(PDBID)==0:
-            break
-        info=PDBID.split()
-        ProteinID=info[0][1:]
-        PrimarySeq=f.readline().strip()
+        PrimarySeq=f.readline().strip()        
         PrimarySeq=PrimarySeq.upper()
         ProteinLen=len(PrimarySeq)
         PrimarySeq=[AA_Dict[e] for e in PrimarySeq]
         PrimarySeq=torch.tensor(PrimarySeq,dtype=torch.long)
-               
+        if With_SS:
+            SecondarySeq=f.readline().strip()
+            SecondarySeq=[SS9_Dict[e] for e in SecondarySeq]
+            SecondarySeq=torch.tensor(SecondarySeq,dtype=torch.long)
+        else:
+            SecondarySeq=None               
         PHI=f.readline().strip().split()
         tmp_list=[]
         for i in range(ProteinLen):
@@ -121,7 +63,7 @@ def load_FastaFile2(FastaFile,IsESM=True):
                 tmp_list.append(float('nan'))
         PSI=torch.tensor(tmp_list,dtype=torch.float)       
                  
-        Node=ProteinNode(ProteinID,ProteinLen,PrimarySeq,None,PHI/180.0,PSI/180.0,None)
+        Node=ProteinNode(ProteinID,ProteinLen,PrimarySeq,SecondarySeq,PHI/180.0,PSI/180.0)
         Data.append(Node)
     f.close()
     return Data
@@ -140,13 +82,13 @@ def load_train_valid_test(IsESM=True):
 
 
 def load_four_testsets(IsESM=True):
-   Test2020_HQ_list=load_FastaFile2('data/data/TEST2020_HQ.txt',IsESM)
+   Test2020_HQ_list=load_FastaFile('data/TEST2020_HQ.txt',IsESM,False)
    Test2020_HQ_list.sort(key=lambda Node: Node.ProteinLen,reverse=False)   
-   CASP12_list=load_FastaFile2('data/data/CASP12.txt',IsESM)
+   CASP12_list=load_FastaFile('data/CASP12.txt',IsESM,False)
    CASP12_list.sort(key=lambda Node: Node.ProteinLen,reverse=False) 
-   CASP13_list=load_FastaFile2('data/data/CASP13.txt',IsESM)
+   CASP13_list=load_FastaFile('data/CASP13.txt',IsESM,False)
    CASP13_list.sort(key=lambda Node: Node.ProteinLen,reverse=False) 
-   CASPFM_list=load_FastaFile2('data/data/CASPFM.txt',IsESM)
+   CASPFM_list=load_FastaFile('data/CASPFM.txt',IsESM,False)
    CASPFM_list.sort(key=lambda Node: Node.ProteinLen,reverse=False)    
    return Test2020_HQ_list, CASP12_list, CASP13_list, CASPFM_list
 
